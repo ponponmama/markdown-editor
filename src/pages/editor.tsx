1ef2cbcd521fd3a9bf75f24aba0242eb8090ac0f
ponponmama <buti1024@gmail.com>
2024-12-17 15:25:52 +0900

    ウェブアプリマニフェスト実装

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { useStateWithStorage } from '../hooks/use_state_with_storage'
-import * as ReactMarkdown from 'react-markdown'
 import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
 import { Link } from 'react-router-dom'
 import { Header } from '../components/header'
+import ConvertMarkdownWorker from 'worker-loader!../worker/convert_markdown_worker'
 
-const { useState } = React
+const convertMarkdownWorker = new ConvertMarkdownWorker()
+const { useState, useEffect } = React
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -18,7 +18,8 @@ const Wrapper = styled.div`
   top: 3rem;
   `
 
-const HeaderArea = styled.div`  position: fixed;
+const HeaderArea = styled.div`  
+  position: fixed;
   right: 0;
   top: 0;
   left: 0;
@@ -46,13 +47,27 @@ const Preview = styled.div`
   top: 0;
   width: 50vw;
 `
-const StorageKey = 'pages/editor:text'
+interface Props {
+  text: string
+  setText: (text: string) => void
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('', StorageKey)
-
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props
   const [showModal, setShowModal] = useState(false)
+  const [html, setHtml] = useState('')
 
+  useEffect(() => {
+    convertMarkdownWorker.onmessage = (event) => {
+      setHtml(event.data.html)
+    }
+  }, [])
+
+  useEffect(() => {
+    convertMarkdownWorker.postMessage(text)
+  }, [text])
+
+  
   return (
     <>
       <HeaderArea>
@@ -71,7 +86,7 @@ export const Editor: React.FC = () => {
           value={text}
         />
         <Preview>
-          <ReactMarkdown>{text}</ReactMarkdown>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
         </Preview>
       </Wrapper>
       {showModal && (
